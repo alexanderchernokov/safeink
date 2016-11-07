48d642818626eed080771ee6e3809dbc139234ee
alexander.c <alexander.chernokov@colmobil.co.il>
2016-11-07 15:15:57 +0200

    add languages values for payments

---
+++ includes/custom_languages/en/payments.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexander.c
+ * Date: 07/11/2016
+ * Time: 15:05
+ */
+$payments_val = array(
+    'outgoing_payments'=>'Outgoing Payments',
+    'income_payments'=>'Income Payments',
+    'payments_history'=>'Payments History',
+    'create_new'=>'Create new payment process',
+    'no_payments_found'=>'No payments found'
+
+);
---
+++ includes/custom_languages/ru/payments.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexander.c
+ * Date: 07/11/2016
+ * Time: 15:05
+ */
+$payments_val = array(
+    'outgoing_payments'=>'Исходящие платежи',
+    'income_payments'=>'Входящие платежи',
+    'payments_history'=>'История платежей',
+    'create_new'=>'Создать новый процесс',
+    'no_payments_found'=>'Не найдено платежных процессов'
+
+);
